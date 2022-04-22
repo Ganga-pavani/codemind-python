fbef18d2a1fe1fd2c5fa1dc9b9d98966875463de
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-04-22 12:00:09 +0530

    s=str(input()) count=0 for character in s:         if character.isdigit():             count=count+1 if count>0:     print("Contains",count,"digit") else:     print("Doesn't contain digit")

---
+++ string_contain_digit_or_not.py
@@ -0,0 +1,12 @@
+s=str(input())
+count=0
+for character in s:
+        if character.isdigit():
+            count=count+1
+if count>0:
+    print("Contains",count,"digit")
+else:
+    print("Doesn't contain digit")
+    
+       
+        
