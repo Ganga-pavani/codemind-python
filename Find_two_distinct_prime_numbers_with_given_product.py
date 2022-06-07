d0ad1db4b3093d1df88fcc1b6e3fbf4eecf17ac3
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-06-07 22:56:25 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) a=[] for i in range(2,n):     if n%i==0 and prime(i):         a.append(i) if a==[]:     print(-1) else:     print(*a)

---
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -0,0 +1,15 @@
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+a=[]
+for i in range(2,n):
+    if n%i==0 and prime(i):
+        a.append(i)
+if a==[]:
+    print(-1)
+else:
+    print(*a)
