956c3ae16040a90e5a4c373d475869301f7fb566
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-10-30 21:38:24 +0530

    for i in range(int(input())):     n=int(input())     arr=list(map(int,input().split()))     c=0     for i in range(n-1):         for j in range(n-i-1):             if arr[j]>arr[j+1]:                 temp=arr[j+1]                 arr[j+1]=arr[j]                 arr[j]=temp                 c+=1     if c==0:         print('0')     else:         k=arr[n-1]-arr[0]         print(k)

---
+++ Optimal_Sorting.py
@@ -0,0 +1,16 @@
+for i in range(int(input())):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    c=0
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                temp=arr[j+1]
+                arr[j+1]=arr[j]
+                arr[j]=temp
+                c+=1
+    if c==0:
+        print('0')
+    else:
+        k=arr[n-1]-arr[0]
+        print(k)
